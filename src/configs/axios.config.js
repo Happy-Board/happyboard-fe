45c12b1ed473bdffaae6e6b767e55486211e0c05
Mai Trần Duy <87484061+duy-mt@users.noreply.github.com>
2024-07-30 16:56:17 +0700

    Update axios.config.js

--- src/configs/axios.config.js
+++ src/configs/axios.config.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'http://47.129.21.145:8000/api/v1',
   withCredentials: false
 })
 
