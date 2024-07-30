96d9d42c4462808653c3df0fe810f4c9cc9ac115
Mai Trần Duy <87484061+duy-mt@users.noreply.github.com>
2024-07-30 16:52:03 +0700

    Update axios.config.js

--- src/configs/axios.config.js
+++ src/configs/axios.config.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL,
+  baseURL: 'http://localhost:8000/api/v1',
   withCredentials: false
 })
 
