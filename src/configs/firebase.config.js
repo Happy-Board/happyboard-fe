9e2acbe8b0de1948e53acd862d8070f29b025f10
Mai Trần Duy <87484061+duy-mt@users.noreply.github.com>
2024-08-02 15:24:31 +0700

    Update firebase.config.js

--- src/configs/firebase.config.js
+++ src/configs/firebase.config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMRNT_ID,
+  apiKey: "AIzaSyDDuDRFfKyrub4CA58RDj3iqqf641R3-Aw",
+  authDomain: "happyboard-123.firebaseapp.com",
+  projectId: "happyboard-123",
+  storageBucket: "happyboard-123.appspot.com",
+  messagingSenderId: "500567881708",
+  appId: "1:500567881708:web:b13454b331fec5de0ab2c9",
+  measurementId: "G-X71685PTG3",
 };
 
 const app = initializeApp(firebaseConfig);
