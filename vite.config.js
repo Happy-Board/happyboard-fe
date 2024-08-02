f42a0812d94101d83665ec2b92b354736c1081ae
Mai Trần Duy <87484061+duy-mt@users.noreply.github.com>
2024-08-02 15:48:22 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default ({mode}) => {
 
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
   return defineConfig({
   plugins: [vue()],
   server: {
