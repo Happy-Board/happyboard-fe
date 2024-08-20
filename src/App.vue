c5e7d7ef345a5c6c52e36cb9e363e8fd54e5ef98
Mai Trần Duy <87484061+duy-mt@users.noreply.github.com>
2024-08-20 16:12:41 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,22 @@ import SidebarComponent from '@/components/SidebarComponent.vue'
 import HeaderSkeleton from './components/Skeletons/HeaderSkeleton.vue';
 
 const route = useRoute()
+// Thêm Google Analytics
+const initializeGA = () => {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-4P7TBE1K7V";
+  document.head.appendChild(script);
 
+  script.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-4P7TBE1K7V');
+  };
+};
+
+initializeGA();
 </script>
 
 <template>
