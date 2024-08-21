e167dd7e5eda20515c26b437f39a04d36fbce514
Mai Trần Duy <87484061+duy-mt@users.noreply.github.com>
2024-08-21 17:10:23 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,21 @@ import SidebarComponent from '@/components/SidebarComponent.vue'
 import HeaderSkeleton from './components/Skeletons/HeaderSkeleton.vue';
 
 const route = useRoute()
+  const initializeGA = () => {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-5FZMKCHBXF";
+  document.head.appendChild(script);
+
+  script.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-5FZMKCHBXF');
+  };
+};
+
+initializeGA();
 
 </script>
 
