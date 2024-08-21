b7a419deee25062e02a66e6dfd30a7504b505ad2
Mai Trần Duy <87484061+duy-mt@users.noreply.github.com>
2024-08-21 16:02:38 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <script setup>
 import { RouterView } from 'vue-router'
 
-
 const initializeGA = () => {
   const script = document.createElement('script');
   script.async = true;
-  script.src = "https://www.googletagmanager.com/gtag/js?id=G-4P7TBE1K7V";
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-5FZMKCHBXF";
   document.head.appendChild(script);
 
   script.onload = () => {
     window.dataLayer = window.dataLayer || [];
     function gtag(){window.dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'G-4P7TBE1K7V');
+    gtag('config', 'G-5FZMKCHBXF');
   };
 };
 
