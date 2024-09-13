9dceca81234c56b3e4d53b453157785531d91fde
Khaipn888 <phanngockhai888@gmail.com>
2024-09-13 14:30:33 +0700

    fix ui skeleton

--- src/components/Skeletons/CardIdeaSkeleton.vue
+++ src/components/Skeletons/CardIdeaSkeleton.vue
@@ -1,12 +1,7 @@
 <template>
-  <div class="grid grid-cols-6 pt-2 pb-4 border-t border-borderColor w-full">
-    <div class="col-span-1 flex flex-col gap-3 mt-1.5 mr-3 text-[12px] items-end">
-      <AnimatedPlaceholder width="100%" height="8px" border-radius="10px" />
-      <AnimatedPlaceholder width="50%" height="8px" border-radius="10px" />
-      <AnimatedPlaceholder width="60%" height="8px" border-radius="10px" />
-    </div>
-    <div class="col-span-5">
-      <div class="title ms-1">
+  <!-- <div class="grid grid-cols-9 pt-2 pb-1 border-t border-borderColor w-full">
+    <div class="col-span-9">
+      <div class="title flex justify-between">
         <AnimatedPlaceholder width="100%" height="25px" border-radius="10px" />
       </div>
       <div class="content mt-2 ms-1">
@@ -30,8 +25,50 @@
         </div>
       </div>
     </div>
+  </div> -->
+  <div class="grid grid-cols-9 pt-2 pb-1 border-t border-borderColor">
+    <div class="col-span-9">
+      <div class="title flex justify-between">
+        <AnimatedPlaceholder width="100%" height="25px" border-radius="10px" />
+      </div>
+      <div class="content mt-2">
+        <div class="content mt-2 mb-1 border-0 break-words line-clamp-3">
+          <AnimatedPlaceholder width="100%" height="60px" border-radius="10px" />
+        </div>
+      </div>
+      <div class="flex flex-row justify-between items-center">
+        <div class="flex items-center gap-3 mt-1.5 text-[12px]">
+          <div class="flex items-center font-medium text-gray-900">
+            <AnimatedPlaceholder width="30px" height="10px" border-radius="10px" />
+          </div>
+          <div class="flex items-center font-medium text-primaryColor">
+            <AnimatedPlaceholder width="30px" height="10px" border-radius="10px" />
+          </div>
+          <div class="flex items-center font-medium text-secondaryColor">
+            <AnimatedPlaceholder width="30px" height="10px" border-radius="10px" />
+          </div>
+        </div>
+
+        <div class="flex justify-end items-center text-[12px]">
+          <AnimatedPlaceholder width="20px" height="20px" border-radius="50%" />
+
+          <AnimatedPlaceholder width="50px" height="8px" border-radius="10px" />
+
+          <div class="flex relative justify-end items-center">
+            <AnimatedPlaceholder width="20px" height="15px" border-radius="10px" />
+
+            <AnimatedPlaceholder width="50px" height="8px" border-radius="10px" />
+          </div>
+
+          <div class="flex">
+            <AnimatedPlaceholder width="100px" height="8px" border-radius="10px" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 <script setup>
 import AnimatedPlaceholder from './AnimatedPlaceholder.vue'
 </script>
--- src/components/Skeletons/DetailIdeaSkeleton.vue
+++ src/components/Skeletons/DetailIdeaSkeleton.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="flex items-start border-b border-borderColor pb-2 w-full">
-    <div class="flex flex-col items-start ps-2 w-full justify-between">
+    <div class="flex flex-col items-start w-full justify-between">
       <AnimatedPlaceholder width="100%" height="40px" border-radius="5px" />
-      <div class="flex justify-between mt-5">
-        <div class="flex items-center">
-          <div class="mr-3">
-            <AnimatedPlaceholder width="50px" height="20px" border-radius="40%" />
-          </div>
+      <div class="flex justify-between mt-5 w-full">
+        <div class="flex items-center">          
           <AnimatedPlaceholder width="20px" height="20px" border-radius="50%" />
           <AnimatedPlaceholder width="50px" height="8px" border-radius="5px" />
           <div class="flex gap-5 ms-5 items-center">
@@ -19,10 +16,13 @@
             <AnimatedPlaceholder width="50px" height="8px" border-radius="5px" />
           </div>
         </div>
+        <div class="">
+            <AnimatedPlaceholder width="80px" height="30px" border-radius="30%" />
+        </div>
       </div>
     </div>
   </div>
-  <div class="border-0 pb-5 border-b mb-3 ps-2 w-full">
+  <div class="border-0 pb-5 border-b mb-3 w-full">
     <AnimatedPlaceholder width="100%" height="150px" border-radius="5px" />
   </div>
 </template>
